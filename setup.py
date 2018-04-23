3d2dc005d90ac5218ba1d163c9f50e032a636df5
AbeIgnocious <k.l.kubat@gmail.com>
2018-04-23 23:36:04 +0200

    add README.md

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
 from setuptools import setup
 
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
 setup(
     name='dpos-deputy',
     version='0.0.1',
     packages=[''],
+    long_description=readme(),
     url='https://github.com/BlockHub/dpos-CLI',
     license='MIT',
     author='Charles',
@@ -12,6 +17,7 @@ setup(
     install_requires=[
         'Click', 'Arky==1.3.1', 'dpostools==0.1.0', 'pid',
         'psycopg2-binary', 'raven', 'simple-crypt', 'pycrypto',
+        'markdown'
     ],
     py_modules=['cli'],
     entry_points='''
